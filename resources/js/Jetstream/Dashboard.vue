7ea4e69e8134554285b5f59b7603b2a0123a9086
john spruijt <johnspruijt@outlook.com>
2022-02-11 14:21:53 +0100

    Guage now adjustable, does not yet auto adjust with temperature data

--- resources/js/Jetstream/Dashboard.vue
+++ resources/js/Jetstream/Dashboard.vue
@@ -4,12 +4,11 @@
             <div>
                 <jet-application-logo class="block h-12 w-auto" />
             </div>
-
             <div class="mt-6 text-gray-500">
                 <div class="grid grid-cols-2 text-2xl">
                     <div v-for="(pond, index) in ponds" :key="pond.id" class="p-3 m-2 border border-grey rounded">
                         <a :href="'/details/'+pond.id">{{ pond.name }}</a> <br>
-                        <div class="gauge">
+                        <div :id="'guage-'+pond.id" class="gauge">
                             <div class="gauge__body">
                                 <div class="gauge__fill"></div>
                                 <div class="gauge__cover"></div>
@@ -38,8 +37,25 @@
                 ponds: this.fishponds[0],
                 temps: this.fishponds[1]
             }
+        }, 
+        methods: {
+            guage: function(id) {
+                console.log(document.getElementById('guage-1'))
+                        }
+                },
+        mounted() {
+           this.fishponds[0].forEach(element => {
+               var guageElement = document.getElementById('guage-'+element.id)
+               var value = 0.75
+               guageElement.querySelector(".gauge__fill").style.transform = `rotate(${
+                  value / 2
+                }turn)`;
+                guageElement.querySelector(".gauge__cover").textContent = `${Math.round(
+                  value * 100
+                )}`;
+           });
         }
-    })
+                })
 
     /**
      * loop through element,
