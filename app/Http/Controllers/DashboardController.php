1d5c16d19f97f85be64ff91778d3b82e7ad62faf
john spruijt <johnspruijt@outlook.com>
2022-03-14 09:58:18 +0100

    changed all to get

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,9 +13,9 @@ class DashboardController extends Controller
     //
 
     function dump() {
-        var_dump(DB::table('tempSensor')->all());
+        var_dump(DB::table('tempSensor')->get());
         echo '<br><br><br>';
-        var_dump(TempSensor::all());
+        var_dump(TempSensor::get());
     }
 
     function index() {
