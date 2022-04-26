1837523514c7c8520732e6df420f21e61ed40293
Gerben <99052865@mydavinci.nl>
2022-04-26 10:35:05 +0200

    dashboard measurement danger zones
    you can now see each of the measurements that are presented on the dashboard with thier dangerzone levels.

--- resources/js/Jetstream/Dashboard.vue
+++ resources/js/Jetstream/Dashboard.vue
@@ -7,15 +7,15 @@
                     <div v-for="(pond) in ponds" :key="pond.id" class="p-3 m-2 border border-grey rounded">
                         <a :href="'/details/'+pond.id">{{ pond.name }}</a> <br>
 
-                        <a :href="'details/'+pond.id+'/oxygen'" class="dashboardLatests">
+                        <a :href="'details/'+pond.id+'/oxygen'" :id="'oxygen'+pond.id" class="dashboardLatests">
                             {{pond.latest_oxygen_level.oxygen_level}} mg/L
                         </a>
 
-                        <a :href="'details/'+pond.id+'/turbidity'" class="dashboardLatests">
+                        <a :href="'details/'+pond.id+'/turbidity'" :id="'turbidity'+pond.id" class="dashboardLatests">
                             {{pond.latest_turbidity_level.ntu}} NTU
                         </a>
 
-                        <a :href="'details/'+pond.id+'/level'" class="dashboardLatests">
+                        <a :href="'details/'+pond.id+'/level'" :id="'waterLevel'+pond.id" class="dashboardLatests">
                             {{pond.latest_water_level.cm}} cm
                         </a>
 
@@ -55,18 +55,43 @@
         mounted() {
             this.fishponds.forEach(fishpond => {
                 var guageElement = document.getElementById('guage-'+fishpond.id)
+                var oxygenText = document.getElementById('oxygen'+fishpond.id)
+                var turbidityText = document.getElementById('turbidity'+fishpond.id)
+                var waterLevelText = document.getElementById('waterLevel'+fishpond.id)
                 var temperature = fishpond.latest_temperature.temperature
+                var oxygen = fishpond.latest_oxygen_level.oxygen_level
+                var turbidity = fishpond.latest_turbidity_level.ntu
+                var waterLevel = fishpond.latest_water_level.cm
                 var value = temperature/80
-                var minDanger;
-                var maxDanger;
+                var minDangerTemp;
+                var maxDangerTemp;
+                var minDangerOxygen;
+                var maxDangerOxygen;
+                var minDangerTurbidity;
+                var maxDangerTurbidity
+                var minDangerWater;
+                var maxDangerWater;
+
                 fishpond.dangerzones.forEach(dangerzone => {
                     if (dangerzone.data_type == 'temperature') {
-                        minDanger = dangerzone.min;
-                        maxDanger = dangerzone.max;
+                        minDangerTemp = dangerzone.min;
+                        maxDangerTemp = dangerzone.max;
+                    }
+                    if (dangerzone.data_type == 'oxygen') {
+                        minDangerOxygen = dangerzone.min;
+                        maxDangerOxygen = dangerzone.max;
+                    }
+                    if (dangerzone.data_type == 'turbidity') {
+                        minDangerTurbidity = dangerzone.min;
+                        maxDangerTurbidity = dangerzone.max;
+                    }
+                    if (dangerzone.data_type == 'level') {
+                        minDangerWater = dangerzone.min;
+                        maxDangerWater = dangerzone.max;
                     }
                 })
-                var minimum = minDanger/80
-                var maximum = 1 - (maxDanger/80)
+                var minimum = minDangerTemp/80
+                var maximum = 1 - (maxDangerTemp/80)
 
                 guageElement.querySelector(".gauge__max").style.transform = `rotate(${
                   maximum / 2 * -1
@@ -89,13 +114,29 @@
                 guageElement.querySelector(".gauge__cover").textContent = `${Math.round(
                   value*80
                 )}°C`;
-                if (temperature < minDanger || temperature > maxDanger) {
+                if (temperature < minDangerTemp || temperature > maxDangerTemp) {
                     guageElement.querySelector(".gauge__fill2").style.background = '#ff0000'
                     guageElement.querySelector(".gauge__cover").style.color = '#ff0000'
-                } else if (temperature > maxDanger - 5 || temperature < minDanger + 5) {
+                } else if (temperature > maxDangerTemp - 5 || temperature < minDangerTemp + 5) {
                     guageElement.querySelector(".gauge__fill2").style.background = '#FFA500'
                     guageElement.querySelector(".gauge__cover").style.color = '#FFA500'
                 }
+                if (oxygen < minDangerOxygen || oxygen > maxDangerOxygen) {
+                    oxygenText.style.color = '#ff0000'
+                } else if (oxygen > maxDangerOxygen - 2 || oxygen < minDangerOxygen + 2) {
+                    oxygenText.style.color = '#FFA500'
+                }
+                if (turbidity < minDangerTurbidity || turbidity > maxDangerTurbidity) {
+                    turbidityText.style.color = '#ff0000'
+                } else if (turbidity > maxDangerTurbidity - 0.5 || turbidity < minDangerTurbidity + 0.5) {
+                    turbidityText.style.color = '#FFA500'
+                }
+                if (waterLevel < minDangerWater || waterLevel > maxDangerWater) {
+                    waterLevelText.style.color = '#ff0000'
+                } else if (waterLevel > maxDangerWater - 10 || waterLevel < minDangerWater + 10) {
+                    waterLevelText.style.color = '#FFA500'
+                }
+                
             })
         }
     })
